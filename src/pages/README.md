eddc10846153fef1dfaa3da276d10c54b761b204
Taric Ov (Samurai) <65824043+taricov@users.noreply.github.com>
2023-08-06 17:10:06 +0300

    Add files via upload

--- src/pages/README.md
+++ src/pages/README.md
@@ -3,7 +3,7 @@
 Routes will be auto-generated for Vue files in this dir with the same file structure.
 Check out [`vite-plugin-pages`](https://github.com/hannoeru/vite-plugin-pages) for more details.
 
-### Path Aliasing
+### Path Aliasing...............................
 
 `~/` is aliased to `./src/` folder.
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,53 +1,53 @@
 <script setup lang="ts">
 // defineComponents({})
 defineOptions({
-name: 'IndexPage',
+  name: 'IndexPage',
 })
-	const user = useUserStore()
+const user = useUserStore()
 const name = ref(user.savedName)
 
 const router = useRouter()
-	function go() {
-		if (name.value)
-			router.push(`/hi/${encodeURIComponent(name.value)}`)
-	}
+function go() {
+  if (name.value)
+    router.push(`/hi/${encodeURIComponent(name.value)}`)
+}
 
 const { t } = useI18n()
-	</script>
+</script>
 
-	<template>
+<template>
+  <!-- <div class="!text-6xl mx-auto my-20 font-bold flex items-center justify-center">D-Booking</div> -->
+  <div
+    href="#"
+    class="mb-7 inline-flex items-center justify-between rounded-full bg-gray-100 px-4 py-1 text-sm text-gray-700 dark:bg-gray-800 hover:bg-gray-200 dark:text-white dark:hover:bg-gray-700"
+    role="alert"
+  >
+    From checkout to global sales tax compliance, companies
+  </div>
 
-	<!-- <div class="!text-6xl mx-auto my-20 font-bold flex items-center justify-center">D-Booking</div> -->
-	<div href="#" class="inline-flex justify-between items-center py-1 px-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">From checkout to global sales tax compliance, companies</div>
+  <div class="option-group">
+    <div class="option-container">
+      <input id="option-1" class="option-input" checked type="radio" name="options">
+      <input id="option-2" class="option-input" type="radio" name="options">
 
-	<div class="option-group">
-	<div class="option-container">
+      <label class="option !bg-gray-800/30" for="option-1">
+        <span class="option__indicator" />
 
-	<input class="option-input" checked id="option-1" type="radio" name="options" />
-	<input class="option-input" id="option-2" type="radio" name="options" />
+        <span class="option__label">
+          <sub class="!text-[0.3em]">One-Time</sub>
+        </span>
+      </label>
 
-	<label class="option !bg-gray-800/30" for="option-1">
-	<span class="option__indicator"></span>
+      <label class="option !bg-gray-800/30" for="option-2">
+        <span class="option__indicator" />
+        <span class="option__label">
+          <sub class="">Range</sub>
+        </span>
+      </label>
+    </div>
+  </div>
 
-	<span class="option__label">
-	<sub class="!text-[0.3em]">One-Time</sub>
-	</span>
-	</label>
-
-	<label class="option !bg-gray-800/30" for="option-2">
-	<span class="option__indicator"></span>
-	<span class="option__label">
-	<sub class="">Range</sub>
-	</span>
-	</label>
-
-	</div>
-	</div>
-
-
-
-
-<!--
+  <!--
 	<div>
 	<div text-4xl>
 	<div i-carbon-campsite inline-block />
@@ -75,172 +75,198 @@ const { t } = useI18n()
 {{ t('button.go') }}
 </button>
 </div>
-</div> 
+</div>
 -->
-<!-- <input :key="i" type="checkbox" for="i in 10" :id=`checkbox_${i}`>{{"checkbox" + i}}</input> -->
-<TheChip />
-<div i-carbon-logo-github/>  
-<div i-carbon-logo-linkedin />
-
-<div i-carbon-language />
-
-
+  <!-- <input :key="i" type="checkbox" for="i in 10" :id=`checkbox_${i}`>{{"checkbox" + i}}</input> -->
+  <TheChip />
 </template>
 
 <route lang="yaml">
 meta:
 layout: home
 </route>
+
 <style scoped>
 html {
-background: linear-gradient(to bottom,#4678F3,#64D6EE);
-	    font-size: 62.5%;
-height: 100%;
-display: flex;
+	background: linear-gradient(to bottom, #4678F3, #64D6EE);
+	font-size: 62.5%;
+	height: 100%;
+	display: flex;
 }
+
 body {
-width: 100%;
-margin: auto;
+	width: 100%;
+	margin: auto;
 }
+
 .option-group {
-width: 90%;
-       max-width: 400px;
-height: 150px;
-position: relative;
-overflow: hidden;
-	  border-radius: 0.25em;
-	  font-size: 4rem;
-margin: 0.2em auto;
-transform: translateZ(0);
+	width: 90%;
+	max-width: 400px;
+	height: 150px;
+	position: relative;
+	overflow: hidden;
+	border-radius: 0.25em;
+	font-size: 4rem;
+	margin: 0.2em auto;
+	transform: translateZ(0);
 }
+
 .option-container {
-display: flex;
-	 justify-content: center;
-	 align-items: stretch;
-width: 120%;
-height: 100%;
-margin: 0 -10%;
+	display: flex;
+	justify-content: center;
+	align-items: stretch;
+	width: 120%;
+	height: 100%;
+	margin: 0 -10%;
 }
+
 .option {
-overflow: hidden;
-flex: 1;
-display: block;
-padding: 0.5em;
-background: #64D6EE;
-position: relative;
-margin: 0em;
+	overflow: hidden;
+	flex: 1;
+	display: block;
+	padding: 0.5em;
+	background: #64D6EE;
+	position: relative;
+	margin: 0em;
 	margin-right: 0.2em;
 	border-radius: 0.25em;
-display: flex;
-	 justify-content: flex-end;
-	 align-items: flex-start;
-	 flex-direction: column;
-cursor: pointer;
-opacity: 0.5;
-	 transition-duration: 0.8s, 0.6s;
-	 transition-property: transform, opacity;
-	 transition-timing-function: cubic-bezier(.98,0,.22,.98), linear;
-	 will-change: transform, opacity;
+	display: flex;
+	justify-content: flex-end;
+	align-items: flex-start;
+	flex-direction: column;
+	cursor: pointer;
+	opacity: 0.5;
+	transition-duration: 0.8s, 0.6s;
+	transition-property: transform, opacity;
+	transition-timing-function: cubic-bezier(.98, 0, .22, .98), linear;
+	will-change: transform, opacity;
 }
+
 .option:last-child {
 	margin-right: 0;
 }
+
 .option__indicator {
-display: block;
-	 transform-origin: left bottom;
-transition: inherit;
-	    will-change: transform;
-position: absolute;
-top: 0.5em;
-right: 0.5em;
-left: 0.5em;
+	display: block;
+	background: #000;
+
+	transform-origin: left bottom;
+	transition: inherit;
+	will-change: transform;
+	position: absolute;
+	top: 0.5em;
+	right: 0.5em;
+	left: 0.5em;
 }
 
-.option__indicator:before, .option__indicator:after {
-
-display: block;
-border: solid 2px #585757;
-border-radius: 50%;
-width: 0.25em;
-height: 0.25em;
-position: absolute;
-top: 0;
-right: 0;
-}
+.option__indicator:before,
 .option__indicator:after {
-@apply bg-white
-background: #000;
-transform: scale(0);
-transition: inherit;
-	    will-change: transform;
-}
-.option-input {
-position: absolute;
-top: 0;
-     z-index: -1;
-visibility: hidden;
-}
-.option__label {
-	@apply after:dark:border-white;
-	 display: block;
-	 width: 100%;
-	 text-transform: uppercase;
-	 font-size: 1.5em;
-	 font-weight: bold;
-	 transform-origin: left bottom;
-	 transform: translateX(20%) scale(0.7);
-	 transition: inherit;
-	 will-change: transform;
-}
- .option__label sub {
-	 font-size: 0.4em;
-}
- .option__label:after {
-	 content: '';
-	 display: block;
-	 border: solid 2px #000;
-	 width: 100%;
-	 transform-origin: 0 0;
-	 transform: scaleX(0.2);
-	 transition: inherit;
-	 will-change: transform;
-}
- .option:last-child .option__label {
-	 transform: translateX(0%) scale(0.7);
-}
- .option:last-child .option__indicator {
-	 transform: translateX(-20%);
-}
- .option-input:checked ~ .option {
-	 transform: translateX(-20%) translateX(0.2em);
-}
- .option-input:checked ~ .option .option__indicator {
-	 transform: translateX(0%);
-}
- .option-input:checked ~ .option .option__label {
-	 transform: translateX(40%) scale(0.7);
-}
- .option-input:first-child:checked ~ .option {
-	 transform: translateX(20%) translateX(-0.2em);
-}
- .option-input:first-child:checked ~ .option .option__indicator {
-	 transform: translateX(-40%);
-}
- .option-input:first-child:checked ~ .option .option__label {
-	 transform: translateX(0%) scale(0.7);
-}
- .option-input:nth-child(1):checked ~ .option:nth-of-type(1), .option-input:nth-child(2):checked ~ .option:nth-of-type(2) {
-	 opacity: 1;
-}
- .option-input:nth-child(1):checked ~ .option:nth-of-type(1) .option__indicator, .option-input:nth-child(2):checked ~ .option:nth-of-type(2) .option__indicator {
-	 transform: translateX(0);
-}
- .option-input:nth-child(1):checked ~ .option:nth-of-type(1) .option__indicator::after, .option-input:nth-child(2):checked ~ .option:nth-of-type(2) .option__indicator::after {
-	 transform: scale(1);
-}
- .option-input:nth-child(1):checked ~ .option:nth-of-type(1) .option__label, .option-input:nth-child(1):checked ~ .option:nth-of-type(1) .option__label::after, .option-input:nth-child(2):checked ~ .option:nth-of-type(2) .option__label, .option-input:nth-child(2):checked ~ .option:nth-of-type(2) .option__label::after {
-	 transform: scale(1);
-}
- 
 
+	display: block;
+	border: solid 2px #585757;
+	border-radius: 50%;
+	width: 0.25em;
+	height: 0.25em;
+	position: absolute;
+	top: 0;
+	right: 0;
+}
+
+.option__indicator:after {
+	@apply bg-white;
+	background: #000;
+	transform: scale(0);
+	transition: inherit;
+	will-change: transform;
+}
+
+.option-input {
+	position: absolute;
+	top: 0;
+	z-index: -1;
+	visibility: hidden;
+}
+
+.option__label {
+	display: block;
+	width: 100%;
+	text-transform: uppercase;
+	font-size: 1.5em;
+	font-weight: bold;
+	transform-origin: left bottom;
+	transform: translateX(20%) scale(0.7);
+	transition: inherit;
+	will-change: transform;
+}
+
+.option__label sub {
+	font-size: 0.4em;
+}
+
+.option__label:after {
+	@apply  dark:border-white;
+
+	content: '';
+	display: block;
+	border: solid 2px #000000;
+	width: 100%;
+	transform-origin: 0 0;
+	transform: scaleX(0.2);
+	transition: inherit;
+	will-change: transform;
+}
+
+.option:last-child .option__label {
+	transform: translateX(0%) scale(0.7);
+}
+
+.option:last-child .option__indicator {
+	transform: translateX(-20%);
+}
+
+.option-input:checked~.option {
+	transform: translateX(-20%) translateX(0.2em);
+}
+
+.option-input:checked~.option .option__indicator {
+	transform: translateX(0%);
+}
+
+.option-input:checked~.option .option__label {
+	transform: translateX(40%) scale(0.7);
+}
+
+.option-input:first-child:checked~.option {
+	transform: translateX(20%) translateX(-0.2em);
+}
+
+.option-input:first-child:checked~.option .option__indicator {
+	transform: translateX(-40%);
+}
+
+.option-input:first-child:checked~.option .option__label {
+	transform: translateX(0%) scale(0.7);
+}
+
+.option-input:nth-child(1):checked~.option:nth-of-type(1),
+.option-input:nth-child(2):checked~.option:nth-of-type(2) {
+	opacity: 1;
+}
+
+.option-input:nth-child(1):checked~.option:nth-of-type(1) .option__indicator,
+.option-input:nth-child(2):checked~.option:nth-of-type(2) .option__indicator {
+	transform: translateX(0);
+}
+
+.option-input:nth-child(1):checked~.option:nth-of-type(1) .option__indicator::after,
+.option-input:nth-child(2):checked~.option:nth-of-type(2) .option__indicator::after {
+	transform: scale(1);
+}
+
+.option-input:nth-child(1):checked~.option:nth-of-type(1) .option__label,
+.option-input:nth-child(1):checked~.option:nth-of-type(1) .option__label::after,
+.option-input:nth-child(2):checked~.option:nth-of-type(2) .option__label,
+.option-input:nth-child(2):checked~.option:nth-of-type(2) .option__label::after {
+	transform: scale(1);
+}
 </style>
